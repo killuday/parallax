5fe4f0efd64a32c3efd0103ad57b2c9ea86441f3
pahariyatri <bhanusharm122@gmail.com>
2022-11-02 10:33:19 +0530

    second commit

--- src/components/TextBox.js
+++ src/components/TextBox.js
@@ -4,13 +4,13 @@ const TextBox = () => {
   return (
     <div className='text-box'>
         <h3>Get High</h3>
-        <p>
-            Hii i am uday i created this parallax website using react.js i started from java
+        <p style={{fontFamily:'Poor Story'}}>
+            Hii i am Uday i created this parallax website using react.js i started from java
             a backend programming language and then learned about databases, 'this is a textbox' 
             between this parallax so you can get to know a little bit about the creator so i was 
             talking about my journey in web development then i shifted towards node.js a javascript 
-            server side language then i combined databases with node.js and then CRUD applications 
-            and more then i shifted towards javascript and its frontend libraries and framworks like
+            server side runtime environment used as server side programming language then i worked on  databases with node.js and then Rest api's
+            and more then i shifted towards javascript's frontend libraries and framworks like
             react.js and next.js vue.js too but i didn't liked it for some reason so i sticked with react
             and its frameworks its still alot to learning and i am still learning thanks!!
         </p>
